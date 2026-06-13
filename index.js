6db590df630ab957bcb8585beef1f10c3aef6913
citclassmahmud-dotcom <abdullah624683@gmail.com>
2026-06-14 00:15:39 +0600

    verification added

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ const client = new MongoClient(uri, {
 app.use(cors())
 app.use(express.json())
 
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+   if(!token){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+
+  console.log(authHeader,'auth header', token, 'token' )
+next()
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -32,13 +47,70 @@ async function run() {
     const applicationCollection = db.collection('application-collection')
     const plansCollection = db.collection('plans')
     const subscriptionCollection = db.collection('subscription_collection')
+    const sessionCollection = db.collection('session')
+
+
+
+    // verification related 
+    const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+   if(!token){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+
+  const query = {token: token}
+  const session = await sessionCollection.findOne(query)
+  const userQuery = {_id : session.userId}
+  const user = await userCollection.findOne(userQuery)
+  req.user = user
+next()
+}
+
+const verifySeeker = async (req, res, next) => {
+  const user = req.user
+  if(user.role !== 'seeker'){
+    return res.status(403).send({message: 'forbidden'})
+  }
+  next()
+}
+
+const verifyRecruiter = async (req, res, next) => {
+  const user = req.user
+  if(user.role !== 'recruiter'){
+    return res.status(403).send({message: 'forbidden'})
+  }
+  next()
+}
+
+const verifyAdmin = async (req, res, next) => {
+  const user = req.user
+  if(user.role !== 'admin'){
+    return res.status(403).send({message: 'forbidden'})
+  }
+  next()
+}
 
     app.get('/', (req, res) => {
       res.send('hireLoop server is running')
     })
  
     // jobs fetching by company ID
-    app.get('/api/jobs', async (req, res) => {
+    app.get('/api/jobs', verifyToken, verifyRecruiter, async (req, res) => {
+    
+      const companyQuery = {recruiterId : req.user._id.toString()}
+      const companyData = await companyCollection.findOne(companyQuery)
+     
+      // verification 
+      if(req.user._id.toString() !== companyData.recruiterId){
+        return res.status(403).send({message: 'forbidden'})
+      }
+
+
+
       const query = {};
       if(req.query.companyId){
         query.companyId = req.query.companyId;
@@ -53,7 +125,7 @@ async function run() {
 )
 
 // job posting 
-    app.post('/api/jobs', async (req, res) => {
+    app.post('/api/jobs', verifyToken, verifyRecruiter, async (req, res) => {
       const job = req.body;
       const newJob = {
         ...job,
@@ -84,16 +156,13 @@ async function run() {
 
   
   // all company data fetching
-  app.get('/api/companies', async (req, res) => {
-    const result = await companyCollection.find().toArray()
-    res.json(result)
-  })
-
- // all jobs data fetching
-  app.get('/api/jobs', async (req, res) => {
-    const result = await jobCollection.find().toArray()
-    res.json(result)
-  })
+app.get('/api/companies', verifyToken, async (req, res) => {
+  const pipeline = [
+    { $skip: 0 }
+  ]
+  const result = await companyCollection.aggregate(pipeline).toArray()
+  res.json(result)
+})
 
   // job details data fetching
   app.get('/api/jobs/:id', async (req, res) => {
@@ -105,7 +174,15 @@ async function run() {
   })
 
   // company data fetching with recruiter ID
-  app.get('/api/myCompany', async (req, res) => {
+  app.get('/api/myCompany', verifyToken, verifyRecruiter, async (req, res) => {
+
+  
+ // verification 
+      if(req.user._id.toString() !== req.query.recruiterId){
+        return res.status(403).send({message: 'forbidden'})
+      }
+
+
     const query = {}
     if(req.query.recruiterId){
       query.recruiterId = req.query.recruiterId;
@@ -117,6 +194,8 @@ async function run() {
   // company data update
   app.patch('/api/myCompany/:id', async (req, res) => {
   const { id } = req.params;
+
+  
   const updatedData = req.body;
   console.log(id, 'id', updatedData, "updatedData")
 
@@ -146,7 +225,7 @@ app.post('/api/applications', async (req, res) => {
 } )
 
 // applications get by applicant id and job id separately
-app.get(`/api/applications`, async (req, res) =>{
+app.get(`/api/applications`, verifyToken, verifySeeker, async (req, res) =>{
   const query = {};
   if(req.query.applicantId){
     query.applicantId = req.query.applicantId;
@@ -154,7 +233,10 @@ app.get(`/api/applications`, async (req, res) =>{
   if(req.query.jobId){
     query.jobId = req.query.jobId
   }
-
+  if(req.user._id.toString() !== req.query.applicantId){
+    return res.status(403).send({message: 'forbidden'})
+  }
+  console.log(req.user, req.query.applicantId)
   const result = await applicationCollection.find(query).toArray();
   res.json(result)
 
@@ -194,7 +276,7 @@ app.get('/api/plans', async (req, res) => {
   })
 
   // company approving 
-  app.patch('/api/companies/:id', async (req, res) => {
+  app.patch('/api/companies/:id', verifyToken, verifyAdmin, async (req, res) => {
        console.log('patch hit', req.params.id, req.body)
     const id = req.params.id;
     const filter = {_id : new ObjectId(id)}
@@ -206,7 +288,7 @@ app.get('/api/plans', async (req, res) => {
        if (result.modifiedCount > 0) {
         res.json({ success: true, message: 'Status updated' })
     } else {
-        res.status(404).json({ success: false, message: 'Company not found' })
+        res.status(403).json({ success: false, message: 'Company not found' })
     }
   })
 
